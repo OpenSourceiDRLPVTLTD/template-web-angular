609cc8a86e4a405911b23f0924f2c3d059080236
V Mahadev <v.mahadev@gmail.com>
2016-07-16 12:11:55 +0530

    'statusbar'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+export class StatusBar{
+
+}
+export class Section{
+ Title:string;
+}
+export class StatusBarSections{
+    Count:number;
+    Sections:Section[];  
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}} - {{sections.Count}}</h1><div> <div *ngFor="let section of sections.Sections"><span>{{section.Id}}</span>:{{section.Title}}</div></div>'
 })
-export class AppComponent { }
+export class AppComponent { 
+title ='App Title';
+sections:StatusBarSections{Count:2,Sections:[{Id:'Alerts',Title:'Alerts'},
+    {Id:'Status':Title:'Status'}];
+
+}
